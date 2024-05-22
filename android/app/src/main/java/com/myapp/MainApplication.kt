30e4f86aad812c7dfd755d54ba24c1715e037059
decozzfx <m.f.r.ozil@gmail.com>
2024-05-22 21:52:47 +0700

    feat(general): initial commit

--- android/app/src/main/java/com/myapp/MainApplication.kt
+++ android/app/src/main/java/com/myapp/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.myapp
+package com.supree
 
 import android.app.Application
 import com.facebook.react.PackageList
--- plugins/index.js
+++ plugins/index.js
@@ -1,6 +1,6 @@
-const prompts = require('prompts');
-const typescriptPlugin = require('./compile-js');
-const printSuccessPlugin = require('./printSuccess');
+const prompts = require("prompts");
+const typescriptPlugin = require("./compile-js");
+const printSuccessPlugin = require("./printSuccess");
 
 // List of plugin to apply (ordered)
 const plugins = [typescriptPlugin, printSuccessPlugin];
@@ -8,7 +8,7 @@ const plugins = [typescriptPlugin, printSuccessPlugin];
 /**
  * @typedef {Object} Plugin
  * @property promptsOptions {Object|null} the plugin prompt property
- * @property apply {Function} the refactoring to apply to the myapp
+ * @property apply {Function} the refactoring to apply to the supree
  */
 
 /**
@@ -20,25 +20,25 @@ const plugins = [typescriptPlugin, printSuccessPlugin];
  * @return {Promise<*>}
  */
 async function applyPlugin(name, plugin, response) {
-	if (!plugin.promptsOptions) {
-		await plugin.apply(null, response);
-		return { [name]: null, ...response };
-	}
-	const { value } = await prompts({ ...plugin.promptsOptions, name: 'value' });
-	await plugin.apply(value, response);
-	return { [name]: value, ...response };
+  if (!plugin.promptsOptions) {
+    await plugin.apply(null, response);
+    return { [name]: null, ...response };
+  }
+  const { value } = await prompts({ ...plugin.promptsOptions, name: "value" });
+  await plugin.apply(value, response);
+  return { [name]: value, ...response };
 }
 
 module.exports = {
-	/**
-	 * Apply all plugin in the order of plugin
-	 * @return {Promise<{apply: function(*=): Promise<void>, name: string, promptsOptions: {onRender(*): void, initial: boolean, name: string, type: string, message: string}|{type?: string, name?: string, message?: string, msg?: *, initial?: boolean, onRender?: function(*): void}}|{name?: string, promptsOptions?: {onRender(*): void, initial: boolean, name: string, type: string, message: string}|{type?: string, name?: string, message?: string, msg?: *, initial?: boolean, onRender?: function(*): void}, apply?: function(*=): Promise<void>}|{apply: function(*, *): Promise<Promise.Promise|Promise<unknown>>, name: string, promptsOptions: null}|{name?: string, promptsOptions?: null, apply?: function(*, *): Promise<Promise.Promise|Promise<unknown>>}>}
-	 */
-	async applyPlugins() {
-		return plugins.reduce(
-			(acc, { name, ...plugin }) =>
-				acc.then(response => applyPlugin(name, plugin, response)),
-			Promise.resolve({})
-		);
-	},
+  /**
+   * Apply all plugin in the order of plugin
+   * @return {Promise<{apply: function(*=): Promise<void>, name: string, promptsOptions: {onRender(*): void, initial: boolean, name: string, type: string, message: string}|{type?: string, name?: string, message?: string, msg?: *, initial?: boolean, onRender?: function(*): void}}|{name?: string, promptsOptions?: {onRender(*): void, initial: boolean, name: string, type: string, message: string}|{type?: string, name?: string, message?: string, msg?: *, initial?: boolean, onRender?: function(*): void}, apply?: function(*=): Promise<void>}|{apply: function(*, *): Promise<Promise.Promise|Promise<unknown>>, name: string, promptsOptions: null}|{name?: string, promptsOptions?: null, apply?: function(*, *): Promise<Promise.Promise|Promise<unknown>>}>}
+   */
+  async applyPlugins() {
+    return plugins.reduce(
+      (acc, { name, ...plugin }) =>
+        acc.then((response) => applyPlugin(name, plugin, response)),
+      Promise.resolve({})
+    );
+  },
 };
